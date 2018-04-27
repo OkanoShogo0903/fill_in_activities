3f0c243e80c8f9c65e7412ebfd212f45cfbf6f2e
okano_shogo <b1546842@planet.kanazawa-it.ac.jp>
2018-04-27 14:15:51 +0900

    後々etcファイルに突っ込むやつら

---
+++ test_space.py
@@ -0,0 +1,8 @@
+import fill_in
+s = "4/11 12:00 ~ 18:00 20:30 ~ 21:00 to:univ,univ, ohayou " 
+#s = "4/11 12:00~18:00 20:00~21:00 ohayou 4/12 10:00~12:00 konbanwa"
+#s = "4"
+#s = '<p><span class="atMention" title="湯田">@yuda13</span> 返信に対する返信(日付書いてないから送信はされないよ、あと送りたくないものは明記すれば送られないよ)</p>'
+s = s + "<p><strong>セカンドタイトル</strong><br/>２つ目の投稿<br/>です</p>"
+fill_in.Comment(s)
+    
